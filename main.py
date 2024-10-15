2ab61cbbbf060d57bfa05cacd0fbd70675e09390
Silvana Melitsko <melitsko067@est.derecho.uba.ar>
2024-10-15 13:53:08 -0300

    Day 29: Getting app.routes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-
-app = Flask("Website")
+# name is a special variable, only run website when script is executed directly
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -8,11 +8,13 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/api/b1/<station>/<date>")
+@app.route("/api/v1/<station>/<date>")
 def about(station, date):
+    # df = pandas...
     temperature = 23
+    # return str(temperature)
     return {"station": station, "date": date, "temperature": temperature}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
